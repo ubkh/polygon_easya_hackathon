6c9e30deb5f7d93b8dc9583ba8fe135719cfb004
Bhavik Glibert <83555685+Bhavik-Gilbert@users.noreply.github.com>
2022-10-22 22:37:32 +0100

    Voting Page Complete

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -7,8 +7,33 @@ import "./App.css";
 class Vote extends Component {
     render() {
         return (
-            <>
+            <>  
+                <div align="center">
                 <h1>Vote</h1>
+
+                <form id="form">
+                    <div class="text">Vote for a charity:</div>
+                    <br/>
+                    <div align="left">
+                    <input type="radio" id="Option" name="catagory" value="Hunger" />
+                    <label for="Hunger">Hunger</label><br />
+                    <input type="radio" id="Option" name="catagory" value="Health" />
+                    <label for="Health">Health</label><br />
+                    <input type="radio" id="Option" name="catagory" value="Education" />
+                    <label for="Education">Education</label><br />
+                    <input type="radio" id="html" name="catagory" value="Environment" />
+                    <label for="Environment">Environment</label><br />
+                    <input type="radio" id="Option" name="catagory" value="Animal" />
+                    <label for="Animal">Animal</label><br />
+                    <input type="radio" id="Option" name="catagory" value="Human Services" />
+                    <label for="Human Services">Human Services</label>
+                    </div>
+
+                    <br/>
+                    <button type="submit" class="btn btn-sm btn-light btn-outline-primary">Vote</button>
+                </form>
+
+                </div>
             </>
         );
     }
