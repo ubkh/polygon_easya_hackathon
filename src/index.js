86756f0718084da3ff7ae25fb1817e1f553caf2f
Bhavik Glibert <83555685+Bhavik-Gilbert@users.noreply.github.com>
2022-10-23 12:34:02 +0100

    Cleanup

--- src/index.js
+++ src/index.js
@@ -4,15 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-// import ColorSchemesExample from "./components/Navbar/navbar";
-// import Header from "./components/Header/header.js";
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
-
-//ReactDOM.render(<ColorSchemesExample />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
